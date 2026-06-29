f674e0febebe91fbdd3496bbfed96dc6cca7fd02
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-06-29 06:24:10 +0530

    Add files via upload

---
+++ vowels_consonants_string.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	int i=0,vowels=0,consonants=0;
+	scanf("%s",&str);
+	while(str[i]!='\0'){
+		if(str[i]>='A' && str[i]<='Z' || str[i]>='a' && str[i]<='z')
+			if(str[i]=='a'|| str[i]=='e'||str[i]=='i'|| str[i]=='o' || str[i]=='u' || str[i]=='A'|| str[i]=='E'||str[i]=='I'|| str[i]=='O' || str[i]=='U'){
+				vowels++;
+			}
+			else{
+				consonants++;
+			}
+			i++;
+	}
+	printf("%d\n",vowels);
+	printf("%d",consonants);
+	return 0;
+}
