d4faa73f5e20b6b4d71f32f3ac0e60369225ef9d
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-07-03 06:21:28 +0530

    Add files via upload

---
+++ compress_string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	int i,count;
+	scanf("%s",&str);
+	for(i=0;str[i]!='\0';i++){
+		count=1;
+		while(str[i]==str[i+1]){
+			count++;
+			i++;
+		}
+		printf("%c = %d \n",str[i],count);
+	}
+	return 0;
+}
