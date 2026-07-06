44187befcfc33da656971bacfdaa1ddddf58a7f5
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-07-06 06:37:07 +0530

    Add files via upload

---
+++ removal_of_digits.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	int i,j=0;
+	char result[100];
+	fgets(str,sizeof(str),stdin);
+	for(i=0;str[i]!='\0';i++){
+		if(!(str[i]>='0' && str[i]<='9')){
+			result[j]=str[i];
+			j++;
+		}
+	}
+	result[j]='\0';
+	printf("%s",result);
+	return 0;
+}
